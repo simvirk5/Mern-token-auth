e88769c6fe810c923bd9ceb3e49172b25af91c16
simvirk5 <simvirk5@gmail.com>
2018-05-08 10:15:06 -0700

    styled navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
 import Nav from './layout/Nav';
@@ -10,10 +9,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Router>
           <div className="container">
             <Nav />
